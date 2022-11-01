44844c9264a01561390c9ef8fdda28c7c51e7650
lengzhao <4889384+lengzhao@users.noreply.github.com>
2022-11-01 17:35:41 +0800

    Update db_test.go

--- db_test.go
+++ db_test.go
@@ -1,6 +1,7 @@
 package contractevent
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -23,6 +24,35 @@ func TestCreateTable(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	item := DBItem{TX: "0x1234", LogIndex: 1, Others: []byte{11, 22}}
+	n, err := InsertItem(db, alias, item)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println("insert item:", n, item)
+	n2, err := InsertItem(db, alias, item)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n2 != n {
+		t.Fatal("different id:", n, n2)
+	}
+
+	item2 := DBItem{TX: "0x1234", LogIndex: 2, Others: []byte{11, 22}}
+	_, err = InsertItem(db, alias, item2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	items, err := ListItems(db, alias, 0, 10)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(items) != 2 {
+		t.Fatal(items)
+	}
+
 }
 
 func TestSetNotifyRecord(t *testing.T) {
